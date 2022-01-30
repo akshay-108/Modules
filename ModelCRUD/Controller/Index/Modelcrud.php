92e2e6d814b2585b4a5b65a179092e76b2a82082
root <akshay.pawar@ambab@gmail.com>
2022-01-30 19:47:39 +0530

    create table in database ubdate table in database and fetch data help of model

---
+++ ModelCRUD/Controller/Index/Modelcrud.php
@@ -0,0 +1,13 @@
+<?php
+namespace Practice\ModelCRUD\Controller\Index;
+
+class Modelcrud extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        echo "hello";
+    }
+}
+
+
+?>
---
+++ ModelCRUD/registration.php
@@ -0,0 +1,4 @@
+<?php
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Practice_ModelCRUD', __DIR__);
