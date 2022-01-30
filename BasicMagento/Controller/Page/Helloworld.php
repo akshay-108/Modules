756226044c778a291595554697bcbf8913725392
root <akshay.pawar@ambab@gmail.com>
2022-01-30 19:38:58 +0530

    Basic module of magento2

---
+++ BasicMagento/Controller/Page/Helloworld.php
@@ -0,0 +1,13 @@
+<?php
+namespace Practice\BasicMagento\Controller\Page;
+
+class Helloworld extends \Magento\Framework\App\Action\Action
+{
+    
+    public function execute()
+	{
+		echo "Hello world";
+    }
+}
+
+?>
