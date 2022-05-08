036a3d1b8719ed0cb35b5741c9cda03a7b7e3a55
root <akshay.pawar@ambab@gmail.com>
2022-05-08 14:21:21 +0530

    deleted all old modules

--- ModelCRUD/registration.php
+++
@@ -1,4 +0,0 @@
-<?php
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Practice_ModelCRUD', __DIR__);
--- ProxieCli/registration.php
+++
@@ -1,4 +0,0 @@
-<?php
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Practice_ProxieCli', __DIR__);
