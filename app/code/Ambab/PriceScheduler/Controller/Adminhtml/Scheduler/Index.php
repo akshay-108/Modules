eaf83a77bdbda0d71454089abbca87fc70f2fc77
akshay <akshay.pawar@ambab.com>
2022-06-23 19:48:06 +0530

    price scheduler module

---
+++ app/code/Ambab/PriceScheduler/Controller/Adminhtml/Scheduler/Index.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Ambab\PriceScheduler\Controller\Adminhtml\Scheduler;
+
+
+class Index extends \Magento\Backend\App\Action
+{
+    protected $resultPageFactory = false;
+
+	public function __construct(
+		\Magento\Backend\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $resultPageFactory
+	)
+	{
+		parent::__construct($context);
+		$this->resultPageFactory = $resultPageFactory;
+	}
+
+	public function execute()
+	{
+		$resultPage = $this->resultPageFactory->create();
+		$resultPage->getConfig()->getTitle()->prepend((__('Price Scheduler')));
+
+		return $resultPage;
+	}
+
+}
+
+
+?>
---
+++ app/code/Ambab/PriceScheduler/Controller/Index/Index.php
@@ -0,0 +1,13 @@
+<?php
+namespace Ambab\PriceScheduler\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        echo "Hello";
+    }
+}
+
+
+?>
