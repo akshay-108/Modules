46c994d7b782ad05a0ea6b309ebe2c8c67cbac64
akshay <akshay.pawar@ambab.com>
2022-06-23 13:01:47 +0530

    create category attribute programmatically

---
+++ app/code/Mageplaza/CategoryAttribute9/registration.php
@@ -0,0 +1,6 @@
+<?php
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Mageplaza_CategoryAttribute9',
+        __DIR__
+    );
